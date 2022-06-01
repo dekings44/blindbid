5f4bbf7b260e6e7ed6945482b0cd6a489382880e
Kingsley Ihemere <dekings44@gmail.com>
2022-06-01 18:37:30 +0100

    updated the blind bidding app

--- blindbids.py
+++ blindbids.py
@@ -5,11 +5,23 @@ bids = {}
 
 bidding_completed = False
 
+
+def find_highest_bidder(bidding_record):
+    highest_bid = 0
+    for bidder in bidding_record:
+        bid_ammount = bidding_record[bidder]
+        if bid_ammount > highest_bid:
+            highest_bid = bid_ammount
+
+            winner = bidder
+
+    print(f'The winner is {winner} with a bid of £{highest_bid}')
+
 while not bidding_completed:
 
     name = input('What is your name?\n')
 
-    price = input('What is your bid? \n£')
+    price = int(input('What is your bid? \n£'))
 
     bids[name] = price
 
@@ -17,5 +29,6 @@ while not bidding_completed:
 
     if continued == "no":
         bidding_completed = True
-
-
+        find_highest_bidder(bids)
+    elif continued == "yes":
+        print('Please continue')
